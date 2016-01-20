cebac2cbf2963c0b2312f2b98e982688af87efb4
Paulosaure <paul.lavoine91@gmail.com>
2016-01-20 19:13:24 +0100

    Initial Commit

---
+++ hotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hotel
+//
+//  Created by Paul Lavoine on 20/01/2016.
+//  Copyright © 2016 Paul Lavoine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
